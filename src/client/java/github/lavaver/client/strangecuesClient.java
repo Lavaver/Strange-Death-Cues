be6410b7354e9e7668f867c94544f0f157d391ac
Lavaver <yansuan_game@qq.com>
2024-08-12 01:45:40 +0800

    Initial commit

---
+++ src/client/java/github/lavaver/client/strangecuesClient.java
@@ -0,0 +1,10 @@
+package github.lavaver.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class strangecuesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/github/lavaver/strangecues.java
@@ -0,0 +1,10 @@
+package github.lavaver;
+
+import net.fabricmc.api.ModInitializer;
+
+public class strangecues implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
